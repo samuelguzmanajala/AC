eea27e98dac290784c4b36337e57297ba5135596
Samuel Guzman <samuelguzmanajala@gmail.com>
2020-12-21 13:03:35 +0100

    hfdsalkhjfkjl

---
+++ cmake-build-debug/CMakeFiles/pgnetica.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pgnetica.dir/fun_s.c.o"
+  "CMakeFiles/pgnetica.dir/gengrupos_s.c.o"
+  "pgnetica"
+  "pgnetica.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pgnetica.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
